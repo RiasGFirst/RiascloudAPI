9d2802a98965d96fcb90053462536854504d3ac8
ErzaScarletFairyTail <gremoryrias69@proton.me>
2023-08-09 21:48:06 +0200

    Test SubReddit Scrapping #11 Creating a DB...

--- test/testReddit3.py
+++ test/testReddit3.py
@@ -1,9 +1,8 @@
 from playwright.sync_api import sync_playwright
+from db.testDB import DataBase
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
-from pprint import pprint
 import requests
-import json
 import time
 import os
 
@@ -12,8 +11,6 @@ load_dotenv()
 reddit_username = os.getenv("REDDIT_USERNAME")
 reddit_password = os.getenv("REDDIT_PASSWORD")
 reddit_verify = os.getenv("REDDIT_CONNECTED")
-reddit_session = os.getenv("REDDIT_SESSION") #Use for dev only
-subredditDB_file = '../db/subreddit.json'
 
 
 #div._3dLmvT0hpACHFxhncqzCOr
@@ -23,7 +20,7 @@ def loginReddit():
     print("Login Reddit")
     with sync_playwright() as p:
 
-        browser = p.chromium.launch(headless=False, slow_mo=50)
+        browser = p.chromium.launch(headless=False)
         page = browser.new_page()
         page.goto('https://www.reddit.com/login')
         page.fill('input#loginUsername', reddit_username)
@@ -60,45 +57,7 @@ def loginReddit():
             return connected, reddit_session_cookie
 
 
-def createJSONBD(file_path):
-    # verify if file exist
-    if os.path.isfile(file_path):
-        print("File Exist")
-    else:
-        with open(file_path, 'w') as f:
-            f.write('{"exist": [],"subreddit": {}}')
-            f.close()
-        print("File created")
-
-
-def whatIsBeforeId(subreddit, file_path):
-    with open(file_path, 'r') as f:
-        data = json.load(f)
-        for sub in data["exist"]:
-            if sub == subreddit:
-                before_id = data['subreddit'][subreddit]['before_id']
-                return before_id
-            else:
-                return None
-
-
-def addBeforeId(subreddit, before_id, file_path):
-    with open(file_path, 'r') as f:
-        data = json.loads(f.read())
-        if subreddit in data['exist']:
-            data['subreddit'][subreddit]['before_id'] = before_id
-            with open(file_path, 'w') as fe:
-                json.dump(data, fe, indent=3)
-            return "Before ID Updated"
-        else:
-            data['exist'].append(subreddit)
-            data['subreddit'][subreddit] = {'before_id': before_id}
-            with open(file_path, 'w') as fw:
-                json.dump(data, fw, indent=3)
-            return "New Subreddit Added"
-
-
-def getSubReddit(subreddit, reddit_session_cookie):
+def getSubReddit(subreddit, reddit_session_cookie, before_id=None):
     url_template = "https://www.reddit.com/r/{}/new.json?t=all{}"
     headers = {
         "User-Agent": "Mozilla/5.0",
@@ -110,10 +69,17 @@ def getSubReddit(subreddit, reddit_session_cookie):
     response = requests.get(url=url, headers=headers)
 
     if response.ok:
-        before_id = whatIsBeforeId(subreddit=subreddit, file_path=subredditDB_file)
         data = response.json()['data']
         new_before = data['children'][0]['data']['name']
-        print(new_before)
+
+        if DataBase().verifySubreddit(subreddit=subreddit):
+            before_id = DataBase().getBeforeId(subreddit=subreddit)
+            print(f"Before ID: {before_id}")
+        else:
+            DataBase().addSubreddit(subreddit=subreddit, before_id=new_before)
+            print("Subreddit Created")
+
+        print(f"New Before ID: {new_before}")
         if before_id == new_before:
             print("No New Posts")
 
@@ -122,8 +88,7 @@ def getSubReddit(subreddit, reddit_session_cookie):
             post_name = pdata['name']
 
             if post_name == before_id:
-                addBeforeId(subreddit=subreddit, before_id=new_before, file_path=subredditDB_file)
-                print("Set New Before")
+                DataBase().updateSubreddit(subreddit=subreddit, before_id=new_before)
                 break
             else:
                 post_id = pdata['id']
@@ -132,7 +97,7 @@ def getSubReddit(subreddit, reddit_session_cookie):
                 post_date = pdata['created_utc']
                 post_url = pdata.get('url_overridden_by_dest')
                 print(post_id, post_title, post_author, post_date, post_url)
-        return print(addBeforeId(subreddit=subreddit, before_id=new_before, file_path=subredditDB_file))
+        return print("Subreddit Connection Successful")
 
     else:
         print("Subreddit Connection Failed")
@@ -140,14 +105,14 @@ def getSubReddit(subreddit, reddit_session_cookie):
 
 
 def main():
-
     connected, reddit_session_cookie = loginReddit()
     print(f"Status: {connected}")
     print(f"Cookie: {reddit_session_cookie}")
     print("=====================================")
+    #reddit_session_cookie = ""
     try:
-        print("SubredditDB File:")
-        createJSONBD(file_path=subredditDB_file)
+        print("Creation of the SubReddit DB:")
+        print("=====================================")
         print("cosplay Subreddit:")
         getSubReddit(subreddit='cosplay', reddit_session_cookie=reddit_session_cookie)
         print('hentai Subreddit:')
